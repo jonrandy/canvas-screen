4624b7414cc47cbd1ff3b5da6ba7326b70093507
Jon Randy <jonrandy@gmail.com>
2022-04-11 12:28:19 +0700

    Small progress

--- js/canvas-screen.js
+++ js/canvas-screen.js
@@ -3,18 +3,29 @@
 
 const
 	IS_LITTLE_ENDIAN = isLittleEndian(),
+	screenDataVal = IS_LITTLE_ENDIAN ? ([r, g, b, a=255]) => ((a<<24) | (b<<16) | (g<<8) | r) :	([r, g, b, a=255]) => ((r<<24) | (g<<16) | (b<<8) | a)
 	IS_FIREFOX = navigator.userAgent.toLowerCase().indexOf('firefox') >= 0
 
 function init(
-	[width, height] = [640, 480]
+	[width, height] = [640, 480],
 	{
 		zoom = 1,
-		blur = false
+		blur = false,
 		parent = document.body,
-		pages = 2,
+		pageCount = 2,
 		background = [0, 0, 0, 255]
 	}
 ) {
+
+	return {
+		width,
+		height,
+		zoom,
+		blur,
+		parent,
+		pageCount,
+		background
+	}
 
 }
 
@@ -46,6 +57,22 @@ function setZoom(zoom, screen = lastScreen) {
 
 function setBlur(blur, screen = lastScreen) {
 
+}
+
+function getBitBuffers(arrayBuffer) {
+	return {
+		raw: arrayBuffer,
+		buf8: new Uint8ClampedArray(buf),
+		buf32: new Uint32Array(buf)
+	}	
+}
+
+function isLittleEndian() {
+	// Determine whether Uint32 is little - or big-endian.
+	let {raw, buf8, buf32} = getBitBuffers(new ArrayBuffer(8))
+	buf32[1] = 0x0a0b0c0d
+	return !(raw[4] === 0x0a && raw[5] === 0x0b && raw[6] === 0x0c &&
+    raw[7] === 0x0d)
 }
 
 
