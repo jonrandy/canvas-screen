e57bd3961e5a678657288135e396ed66713af6bc
Jon Randy <jonrandy@gmail.com>
2022-06-05 00:14:41 +0700

    chg

--- index.js
+++ index.js
@@ -13,16 +13,17 @@ let a=0, b=1
 
 let line = new Uint32Array(wd)
 
-const bar = []
+const bar = [], barb=[]
 for (let j=1;j<=8;j++) {
 	bar.push(s.pixelValue([j*4, j*0, j*11]))
+	barb.push(s.pixelValue([j*0, j*11, j*4]))
 }
 let black = s.pixelValue([0,0,0])
 
 
 
 let buffer = s.pixelBuffer()
-let x, ang, ang2, ang3, mul
+let x,x2, ang, ang2, ang3, mul
 
 ;
 (async ()=>{
@@ -33,13 +34,15 @@ let x, ang, ang2, ang3, mul
 		line.fill(black)
 
 		for (let i=0; i<ht; i++) {
-			ang = n*2+i*2
+			ang = n*2+i
 			ang2 = n*2 - i
 			ang3 = (n+180)*4-i*3
 			mul = 40 + ~~(Math.sin((ang2+ang3)/180*3.142)*40)
 			x = ~~(160 + (Math.sin(ang/180*3.142)*mul) + (Math.sin(ang3/180*3.142)*40))
+			x2 = ~~(160 + (Math.sin((ang2+x)/180*3.142)*40) + (Math.sin(ang2*2/180*3.142)*40))
 
 			line.set(bar, x)
+			line.set(barb, x2)
 			buffer.set(line, (wd*i))
 		}
 
